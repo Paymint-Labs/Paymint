e1bf036d074dbeca3d16cf2c24c745e42e2cdfa1
Rohan Kapur <rohankapurdev@outlook.com>
2020-05-20 07:12:03 -0400

    Work on transaction detail views

--- lib/components/list_tile_components.dart
+++ lib/components/list_tile_components.dart
@@ -1,18 +1,25 @@
 /// ListTile components for the Activity and Security Views inside the BitcoinView widget
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:animations/animations.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:paymint/components/animated_gradient.dart';
 import 'package:paymint/models/models.dart';
+import 'package:toast/toast.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ActiveOutputTile extends StatefulWidget {
   final String name;
   final String currentValue;
   final String blockHeight;
 
-  ActiveOutputTile({Key key, @required this.name, @required this.currentValue, @required this.blockHeight})
+  ActiveOutputTile(
+      {Key key,
+      @required this.name,
+      @required this.currentValue,
+      @required this.blockHeight})
       : super(key: key);
 
   @override
@@ -26,21 +33,9 @@ class _ActiveOutputTileState extends State<ActiveOutputTile> {
   final String _blockHeight;
 
   final List<Gradient> _sweepGradients = [
-    // SweepGradient(colors: [
-    //   Colors.blueAccent,
-    //   Colors.lightBlueAccent,
-    //   Colors.blueAccent
-    // ]),
-    SweepGradient(colors: [
-      Colors.lightBlueAccent,
-      Colors.blue,
-      Colors.lightBlueAccent
-    ]),
-    SweepGradient(colors: [
-      Colors.cyan,
-      Colors.lightBlueAccent,
-    ]),
-    
+    SweepGradient(colors: [Colors.blueAccent, Colors.blue, Colors.blueAccent]),
+    SweepGradient(
+        colors: [Colors.cyanAccent, Colors.lightBlueAccent, Colors.cyanAccent]),
   ];
 
   _ActiveOutputTileState(this._name, this._currentValue, this._blockHeight);
@@ -54,7 +49,7 @@ class _ActiveOutputTileState extends State<ActiveOutputTile> {
       leading: CircleAvatar(
         child: ClipRRect(
           child: AnimatedGradientBox(_sweepGradients, Curves.bounceInOut),
-          borderRadius: BorderRadius.circular(30),
+          borderRadius: BorderRadius.circular(15),
         ),
       ),
       onTap: () {},
@@ -62,13 +57,16 @@ class _ActiveOutputTileState extends State<ActiveOutputTile> {
   }
 }
 
-
 class InactiveOutputTile extends StatefulWidget {
   final String name;
   final String currentValue;
   final String blockHeight;
 
-  InactiveOutputTile({Key key, @required this.name, @required this.currentValue, @required this.blockHeight})
+  InactiveOutputTile(
+      {Key key,
+      @required this.name,
+      @required this.currentValue,
+      @required this.blockHeight})
       : super(key: key);
 
   @override
@@ -82,16 +80,8 @@ class _InactiveOutputTileState extends State<InactiveOutputTile> {
   final String _blockHeight;
 
   final List<Gradient> _sweepGradients = [
-    SweepGradient(colors: [
-      Colors.pink,
-      Colors.pinkAccent,
-      Colors.pink
-    ]),
-    SweepGradient(colors: [
-      Colors.red,
-      Colors.redAccent,
-    ]),
-    
+    SweepGradient(colors: [Colors.pink, Colors.pinkAccent, Colors.pink]),
+    SweepGradient(colors: [Colors.red, Colors.redAccent, Colors.red]),
   ];
 
   _InactiveOutputTileState(this._name, this._currentValue, this._blockHeight);
@@ -105,7 +95,52 @@ class _InactiveOutputTileState extends State<InactiveOutputTile> {
       leading: CircleAvatar(
         child: ClipRRect(
           child: AnimatedGradientBox(_sweepGradients, Curves.bounceInOut),
-          borderRadius: BorderRadius.circular(30),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+      onTap: () {},
+    );
+  }
+}
+
+class LoadingOutputTile extends StatefulWidget {
+  final String currentValue;
+  final String blockTime;
+
+  LoadingOutputTile(
+      {Key key, @required this.currentValue, @required this.blockTime})
+      : super(key: key);
+
+  @override
+  _LoadingOutputTileState createState() =>
+      _LoadingOutputTileState(currentValue, blockTime);
+}
+
+class _LoadingOutputTileState extends State<LoadingOutputTile> {
+  final String _currentValue;
+  final String _blocktime;
+
+  final List<Gradient> _sweepGradients = [
+    SweepGradient(colors: [Colors.purple, Colors.purpleAccent, Colors.purple]),
+    SweepGradient(colors: [
+      Colors.deepPurple,
+      Colors.deepPurpleAccent,
+      Colors.deepPurple
+    ]),
+  ];
+
+  _LoadingOutputTileState(this._currentValue, this._blocktime);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('Pending output'),
+      subtitle: Text(_blocktime),
+      trailing: Text(_currentValue),
+      leading: CircleAvatar(
+        child: ClipRRect(
+          child: AnimatedGradientBox(_sweepGradients, Curves.bounceInOut),
+          borderRadius: BorderRadius.circular(15),
         ),
       ),
       onTap: () {},
@@ -119,10 +154,12 @@ class IncomingTransactionListTile extends StatefulWidget {
   final String currentValue;
 
   @override
-  _IncomingTransactionListTileState createState() => _IncomingTransactionListTileState();
+  _IncomingTransactionListTileState createState() =>
+      _IncomingTransactionListTileState();
 }
 
-class _IncomingTransactionListTileState extends State<IncomingTransactionListTile> {
+class _IncomingTransactionListTileState
+    extends State<IncomingTransactionListTile> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
@@ -141,10 +178,12 @@ class OutgoingTransactionListTile extends StatefulWidget {
   final String currentValue;
 
   @override
-  _OutgoingTransactionListTileState createState() => _OutgoingTransactionListTileState();
+  _OutgoingTransactionListTileState createState() =>
+      _OutgoingTransactionListTileState();
 }
 
-class _OutgoingTransactionListTileState extends State<OutgoingTransactionListTile> {
+class _OutgoingTransactionListTileState
+    extends State<OutgoingTransactionListTile> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
@@ -158,7 +197,8 @@ class _OutgoingTransactionListTileState extends State<OutgoingTransactionListTil
 }
 
 class SendListTile extends StatefulWidget {
-  SendListTile({Key key, this.amount, this.currentValue, this.previousValue, this.tx})
+  SendListTile(
+      {Key key, this.amount, this.currentValue, this.previousValue, this.tx})
       : super(key: key);
 
   final String amount;
@@ -179,7 +219,7 @@ class _SendListTileState extends State<SendListTile> {
     return OpenContainer(
       transitionType: this._transitionType,
       openBuilder: (BuildContext _, VoidCallback openContainer) {
-        return _DetailsPage(widget.tx);
+        return _SendDetailsPage(widget.tx);
       },
       tappable: true,
       closedShape: const RoundedRectangleBorder(),
@@ -234,7 +274,7 @@ class _ReceiveListTileState extends State<ReceiveListTile> {
     return OpenContainer(
       transitionType: this._transitionType,
       openBuilder: (BuildContext _, VoidCallback openContainer) {
-        return _DetailsPage(widget.tx);
+        return _ReceiveDetailsPage(widget.tx);
       },
       tappable: true,
       closedShape: const RoundedRectangleBorder(),
@@ -298,17 +338,15 @@ class PurchaseListTile extends StatelessWidget {
   }
 }
 
-
-/// Widget for the default view for transaction details
-class _DetailsPage extends StatefulWidget {
+class _SendDetailsPage extends StatefulWidget {
   final Transaction _tx;
-  
-  _DetailsPage(this._tx);
+
+  _SendDetailsPage(this._tx);
   @override
-  __DetailsPageState createState() => __DetailsPageState();
+  __SendDetailsPageState createState() => __SendDetailsPageState();
 }
 
-class __DetailsPageState extends State<_DetailsPage> {
+class __SendDetailsPageState extends State<_SendDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -356,20 +394,122 @@ class __DetailsPageState extends State<_DetailsPage> {
               onTap: () {},
             ),
             ListTile(
-              title: Text('txid'),
-              trailing: Text(widget._tx.txid),
+              title: Text('Fee paid:'),
+              trailing: Text(widget._tx.fees.toString() + ' sats'),
               onTap: () {},
             ),
+            ListTile(
+                title: Text('Copy transaction ID',
+                    style: TextStyle(color: Colors.blue)),
+                onTap: () {
+                  Clipboard.setData(new ClipboardData(text: widget._tx.txid));
+                  Toast.show('ID copied to clipboard', context, duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+                }),
+            ListTile(
+              title: Text('Verify on blockchain',
+                  style: TextStyle(color: Colors.blue)),
+              onTap: () {
+                _launchTransactionUrl(context, widget._tx.txid);
+              },
+            )
           ],
         ));
   }
+}
 
-  String _buildDateTimeForTx(int timestamp) {
-    final DateTime time = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
-    return time.toLocal().toString().substring(0, 16);
+class _ReceiveDetailsPage extends StatefulWidget {
+  final Transaction _tx;
+
+  _ReceiveDetailsPage(this._tx);
+  @override
+  __ReceiveDetailsPageState createState() => __ReceiveDetailsPageState();
+}
+
+class __ReceiveDetailsPageState extends State<_ReceiveDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Transaction details',
+            style: GoogleFonts.rubik(
+              textStyle: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+          backgroundColor: Colors.black,
+          elevation: 10,
+        ),
+        body: ListView(
+          children: <Widget>[
+            Container(
+                height: MediaQuery.of(context).size.width / 2,
+                color: Colors.black,
+                child: Center(
+                    child: FlareActor('assets/rive/success.flr',
+                        animation: 'Untitled'))),
+            ListTile(
+              title: Text('Date/Time:'),
+              trailing: Text(_buildDateTimeForTx(widget._tx.timestamp)),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Action:'),
+              trailing: Text(widget._tx.txType),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Amount:'),
+              trailing: Text(_extractBtcFromSatoshis(widget._tx.amount)),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Worth now:'),
+              trailing: Text(widget._tx.worthNow),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Worth when received:'),
+              trailing: Text(widget._tx.worthAtBlockTimestamp),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Fee paid:'),
+              trailing: Text(widget._tx.fees.toString() + ' sats'),
+              onTap: () {},
+            ),
+            ListTile(
+                title: Text('Copy transaction ID',
+                    style: TextStyle(color: Colors.blue)),
+                onTap: () {
+                  Clipboard.setData(new ClipboardData(text: widget._tx.txid));
+                  Toast.show('ID copied to clipboard', context, duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+                }),
+            ListTile(
+              title: Text('Verify on blockchain',
+                  style: TextStyle(color: Colors.blue)),
+              onTap: () {
+                _launchTransactionUrl(context, widget._tx.txid);
+              },
+            )
+          ],
+        ));
   }
+}
 
-  String _extractBtcFromSatoshis(int satoshis) {
-    return (satoshis / 100000000).toString() + ' BTC';
+String _buildDateTimeForTx(int timestamp) {
+  final DateTime time = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
+  return time.toLocal().toString().substring(0, 16);
+}
+
+String _extractBtcFromSatoshis(int satoshis) {
+  return (satoshis / 100000000).toString() + ' BTC';
+}
+
+void _launchTransactionUrl(BuildContext context, String txid) async {
+  final String url = 'https://blockstream.info/tx/' + txid;
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    Toast.show('Cannot launch url', context, duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
   }
 }
