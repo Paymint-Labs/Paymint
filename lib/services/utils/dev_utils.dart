035d33bb9fa9de92c0481646ab899dbaedfdc059
Rohan Kapur <rohankapurdev@outlook.com>
2020-06-09 01:48:41 -0400

    ...

--- lib/services/utils/dev_utils.dart
+++ lib/services/utils/dev_utils.dart
@@ -8,7 +8,6 @@ static debugPrintWalletState() async {
     final ca = wallet.get('changeAddresses');
     final ri = wallet.get('receivingIndex');
     final ci = wallet.get('changeIndex');
-    final txCount = wallet.get('transaction_count');
 
     final secureStore = new FlutterSecureStorage();
     final mnemonic = await secureStore.read(key: 'mnemonic');
@@ -17,7 +16,6 @@ static debugPrintWalletState() async {
     ===========================================================================
     Current (external) receiving index, array: $ri, $ra\n
     Current (internal) change index, array: $ci, $ca\n
-    Transaction count: $txCount\n
     ===========================================================================
     BIP39 seed phrase: $mnemonic
     """);
